f3dd58a3535e9985e43b8c97ee9ec155f7923733
InnaLik <110997246+InnaLik@users.noreply.github.com>
2024-05-14 15:57:04 +0300

    получение праздника по ид месяца и дня

--- about_holiday/see_holiday/router.py
+++ about_holiday/see_holiday/router.py
@@ -9,11 +9,13 @@ from about_holiday.see_holiday.schema import AddHoliday
 router = APIRouter(prefix='/holiday',
                    tags=['Get_Holiday'])
 
+
 @router.get("/")
 async def get_holiday(sessions: AsyncSession = Depends(get_async_session)):
     query = select(Holiday)
     result = await sessions.execute(query)
     return result.mappings().all()
+
 
 @router.post("/")
 async def add_holiday(holiday: AddHoliday, sessions: AsyncSession = Depends(get_async_session)):
@@ -21,3 +23,19 @@ async def add_holiday(holiday: AddHoliday, sessions: AsyncSession = Depends(get_
     await sessions.execute(stmt)
     await sessions.commit()
     return {"status": "success"}
+
+
+@router.get("/month/")
+async def get_holiday_month(id_m: int, sessions: AsyncSession = Depends(get_async_session)):
+    query = select(Holiday).where(Holiday.id_month == id_m).order_by(Holiday.id_day)
+    result = await sessions.execute(query)
+    return {"status": "success",
+            "result": result.mappings().all()}
+
+
+@router.get("/{month}/{day}/")
+async def get_holiday_month(month: int, day: int, sessions: AsyncSession = Depends(get_async_session)):
+    query = select(Holiday).where(Holiday.id_month == month).where(Holiday.id_day == day).order_by(Holiday.id_day)
+    result = await sessions.execute(query)
+    return {"status": "success",
+            "result": result.mappings().all()}
